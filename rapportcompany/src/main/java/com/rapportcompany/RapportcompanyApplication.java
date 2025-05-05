50856a416786b2ddc5a91441fa334cfa31e39565
772vj <772vjrvj@gmail.com>
2025-05-05 17:54:13 +0900

    첫 커밋

---
+++ rapportcompany/src/main/java/com/rapportcompany/RapportcompanyApplication.java
@@ -0,0 +1,13 @@
+package com.rapportcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapportcompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapportcompanyApplication.class, args);
+	}
+
+}
